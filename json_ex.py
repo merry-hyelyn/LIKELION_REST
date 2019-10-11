e0535bd049c747576a315924d2c6db65f4d220aa
merryn <h1335@naver.com>
2019-10-11 23:38:47 +0900

    Add comment

--- json_ex.py
+++ json_ex.py
@@ -9,11 +9,14 @@ diary = {
 print(diary)
 print(type(diary))
 
+# dictionary -> 문자열
 diary_s = json.dumps(diary)
 
 print(diary_s)
 print(type(diary_s))
 
+# 문자열 -> dictionary
 diary_back = json.loads(diary_s)
+
 print(diary_back)
 print(type(diary_back))
